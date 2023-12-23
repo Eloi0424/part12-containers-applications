5f8afa3b1202762c8f1e3a5a6c028f1d83d56794
Eloi0424 <beichu0424@qq.com>
2023-12-23 11:48:09 +0800

    exercise 12.7

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -35,12 +35,18 @@ singleRouter.delete('/', async (req, res) => {
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  res.json(req.todo)
+  res.sendStatus(200);
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  if (req.body.text) req.todo.text = req.body.text
+  if (req.body.done !== undefined) req.todo.done = req.body.done
+  await req.todo.save()
+  res.json(req.todo)
+  res.sendStatus(200);
+
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
